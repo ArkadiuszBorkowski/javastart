9a9352e82ef4275b086932bafb53441a6b66ff6d
ArkadiuszBorkowski <borkowskiarkadiusz91@gmail.com>
2022-08-12 13:36:35 +0200

    iKurs JavaStart - typy danych

---
+++ src/DataTypes.java
@@ -0,0 +1,17 @@
+class DataTypes {
+    public static void main(String[] args) {
+        System.out.print("Liczba int o wartości do 4 bajtów: ");
+        System.out.println(25);
+
+        System.out.print("Liczba double: ");
+        System.out.print(25.75);
+        System.out.print('\n');
+
+        System.out.print("Znak A: ");
+        System.out.println('A');
+
+        System.out.print("Czy uczę się programowania? ");
+        System.out.println(true);
+    }
+}
+
